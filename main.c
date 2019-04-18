a811cfaa9c03d338a85436c5cd717a3e67a207aa
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-18 03:45:05 +0000

    LOST WORKSPACE!!
    This is equivalent to my week 6 lab commit because I lost my che workspace and had to start over. I initialised the variables and now need to create a function to execute the rotation cipher

--- main.c
+++ main.c
@@ -2,5 +2,14 @@
 
 int main() {
 
+int key;
+    char *text[];
+    
+        printf("enter key:");
+        scanf("%d", &key);
+
+        printf("enter encryption:");
+        scanf("*%s");
+        
   return 0;
 }
