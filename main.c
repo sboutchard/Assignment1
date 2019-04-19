242eb1f71253cde2e3cffda787001fb5d5474539
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-19 05:31:47 +0000

    Previous code failed to save again, but I copied and pasted it elsewhere. I was able to paste the code into che. I implemented a function called 'cipher'.

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-
 int main() {
 
-int key;
-    char *text[];
+    int key;
+    char text[100];
     
-        printf("enter key:");
+        printf("Enter key:");
         scanf("%d", &key);
 
-        printf("enter encryption:");
-        scanf("*%s");
+        printf("Enter text to encrypt (CAPS ONLY):");
+        scanf("%s", text);
         
+        cipher(text, key);
+         
   return 0;
+}
+
+void cipher(char text[], int key) {
+    
+    for(int i=0; text[i] != '\0'; i++)
+        text[i] = 65 + (text[i]-65+key)%26;
+        printf("Encrypted text: %s\n", text);
 }
