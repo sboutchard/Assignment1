4e5e7b86c2f8acfb84473075758a9f364c7554b7
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-19 09:37:45 +0000

    update

--- Sub_encryption.c
+++ Sub_encryption.c
@@ -3,6 +3,10 @@
 
 int main()
 {
+    char text[50]
+    printf("Enter text to encrypt (CAPS ONLY):");
+    scanf("%s", text);
+    
     FILE *input;
     input=*fopen(*File_Encrypt, "r");
         while(!feof(input)) {
