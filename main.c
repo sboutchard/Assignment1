cec99d344944aafe03c7a981d6c126f932bd9f8d
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-19 05:34:21 +0000

    Revert "Previous code failed to save again, but I copied and pasted it elsewhere. I was able to paste the code into che. I implemented a function called 'cipher'."
    This reverts commit 242eb1f71253cde2e3cffda787001fb5d5474539.

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
+#include <stdio.h>
+
 int main() {
 
-    int key;
-    char text[100];
+int key;
+    char *text[];
     
-        printf("Enter key:");
+        printf("enter key:");
         scanf("%d", &key);
 
-        printf("Enter text to encrypt (CAPS ONLY):");
-        scanf("%s", text);
+        printf("enter encryption:");
+        scanf("*%s");
         
-        cipher(text, key);
-         
   return 0;
-}
-
-void cipher(char text[], int key) {
-    
-    for(int i=0; text[i] != '\0'; i++)
-        text[i] = 65 + (text[i]-65+key)%26;
-        printf("Encrypted text: %s\n", text);
 }
