d69a5511030c9a488faaac219f232be70f7f6719
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-22 02:50:16 +0000

    updates

--- Sub_encryption.c
+++ Sub_encryption.c
@@ -1,39 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void cipher(char text[], int i);
+
 int main()
 {
-    *char text[50]
+    *char text[80]
     int i=0;
     
     FILE *input;
     input=*fopen(*File_Encrypt, "r");
-        while(!feof(input)) {
     
-            case "A" *char text[i]="T";break;
-            case "B" *char text[i]="S";break;
-            case "C" *char text[i]="O";break;
-            case "D" *char text[i]="F";break;
-            case "E" *char text[i]="U";break;
-            case "F" *char text[i]="D";break;
-            case "G" *char text[i]="W";break;
-            case "H" *char text[i]="R";break;
-            case "I" *char text[i]="N";break;
-            case "J" *char text[i]="L";break;
-            case "K" *char text[i]="Z";break;
-            case "L" *char text[i]="J";break;
-            case "M" *char text[i]="X";break;
-            case "N" *char text[i]="I";break;
-            case "O" *char text[i]="C";break;
-            case "P" *char text[i]="Y";break;
-            case "Q" *char text[i]="V";break;
-            case "R" *char text[i]="H";break;
-            case "S" *char text[i]="B";break;
-            case "T" *char text[i]="A";break;
-            case "U" *char text[i]="E";break;
-            case "V" *char text[i]="Q";break;
-            case "W" *char text[i]="G";break;
-            case "X" *char text[i]="M";break;
-            case "Y" *char text[i]="P";break;
-            case "Z" *char text[i]="K";break;
-    
+    if (inputFile == NULL) {
+        printf("Failed to open\n");
+        
+        cipher(text, i);
+        
+        return(0);
+    }
+
+    void cipher(char text[], int i) {  
+    switch (text) { 
+        while (text[i] != '\0') { 
+            case "A": char text[i]="T"; break;
+            case "B": char text[i]="S"; break;
+            case "C": char text[i]="O"; break;
+            case "D": char text[i]="F"; break;
+            case "E": char text[i]="U"; break;
+            case "F": char text[i]="D"; break;
+            case "G": char text[i]="W"; break;
+            case "H": char text[i]="R"; break;
+            case "I": char text[i]="N"; break;
+            case "J": char text[i]="L"; break;
+            case "K": char text[i]="Z"; break;
+            case "L": char text[i]="J"; break;
+            case "M": char text[i]="X"; break;
+            case "N": char text[i]="I"; break;
+            case "O": char text[i]="C"; break;
+            case "P": char text[i]="Y"; break;
+            case "Q": char text[i]="V"; break;
+            case "R": char text[i]="H"; break;
+            case "S": char text[i]="B"; break;
+            case "T": char text[i]="A"; break;
+            case "U": char text[i]="E"; break;
+            case "V": char text[i]="Q"; break;
+            case "W": char text[i]="G"; break;
+            case "X": char text[i]="M"; break;
+            case "Y": char text[i]="P"; break;
+            case "Z": char text[i]="K"; break;
+                
+                i++;
+                printf("Encrypted text: %s\n", text);
+            }    
+        } 
+    }
