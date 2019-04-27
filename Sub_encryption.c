d978ef38e33d065407bd398d9045f87c7ca84a19
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-27 11:09:08 +0000

    updated code

--- Sub_encryption.c
+++ Sub_encryption.c
@@ -1,58 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cipher(char text[], int i);
+void encrypt(char text[]);
+void decrypt(char text[]);
 
 int main()
 {
-    *char text[80]
-    int i=0;
+    char text[1000];
+    
+    //printf("Enter text to encrypt (CAPS ONLY):"); >>if not using FILE I/O
+    //scanf("%[^\n]s", text);
     
     FILE *input;
     input=*fopen(*File_Encrypt, "r");
+    while (text = fgetc(inputFile)) != EOF) {
+    }
     
-    if (inputFile == NULL) {
-        printf("Failed to open\n");
-        
-    else cipher(text, i);
-        
-        return(0);
-    }
+    encrypt(text);
+    
+    printf("Encrypted text: %s\n", text);
+    
+    decrypt(text); 
+    
+    printf("Decrypted text: %s\n", text);
+    
+    return 0;
+}
 
-    void cipher(char text[], int i) {  
-        while (text = fgetc(inputFile)) != EOF) {
-            switch (text) {
-                
-            case "A": char text[i]="T"; break;
-            case "B": char text[i]="S"; break;
-            case "C": char text[i]="O"; break;
-            case "D": char text[i]="F"; break;
-            case "E": char text[i]="U"; break;
-            case "F": char text[i]="D"; break;
-            case "G": char text[i]="W"; break;
-            case "H": char text[i]="R"; break;
-            case "I": char text[i]="N"; break;
-            case "J": char text[i]="L"; break;
-            case "K": char text[i]="Z"; break;
-            case "L": char text[i]="J"; break;
-            case "M": char text[i]="X"; break;
-            case "N": char text[i]="I"; break;
-            case "O": char text[i]="C"; break;
-            case "P": char text[i]="Y"; break;
-            case "Q": char text[i]="V"; break;
-            case "R": char text[i]="H"; break;
-            case "S": char text[i]="B"; break;
-            case "T": char text[i]="A"; break;
-            case "U": char text[i]="E"; break;
-            case "V": char text[i]="Q"; break;
-            case "W": char text[i]="G"; break;
-            case "X": char text[i]="M"; break;
-            case "Y": char text[i]="P"; break;
-            case "Z": char text[i]="K"; break;
-            default: printf("Error with reading file");
-            
-                i++;
-                printf("Encrypted text: %s\n", text);
-            }    
-        } 
+
+void encrypt(char text[]) {
+    const char def[] = "TSOFUDWRNLZJXICYVHBAEQGMPK";
+    for(int i=0; text[i] != '\0'; i++){
+        
+        if( text[i] >= 'A' && text[i] <= 'Z'){
+            text[i] = def[ (int)text[i] - (int)'A'];
+        }else if(text[i] == ' '){
+            //NOTHING
+        }else{
+            printf("Error with reading file\n");
+            return;
+        }
+        
     }
+}
+
+void decrypt(char text[]) {
+    const char def[] = "TSOFUDWRNLZJXICYVHBAEQGMPK";
+    for(int i=0; text[i] != '\0'; i++) {
+        switch (text[i]){
+            case 'T' : text[i] = 'A';
+            break;
+            case 'S' : text[i] = 'B';
+            break;
+            case 'O' : text[i] = 'C';
+            break;
+            case 'F' : text[i] = 'D';
+            break;
+            case 'U' : text[i] = 'E';
+            break;
+            case 'D' : text[i] = 'F';
+            break;
+            case 'W' : text[i] = 'G';
+            break;
+            case 'R' : text[i] = 'H';
+            break;
+            case 'N' : text[i] = 'I';
+            break;
+            case 'L' : text[i] = 'J';
+            break;
+            case 'Z' : text[i] = 'K';
+            break;
+            case 'J' : text[i] = 'L';
+            break;
+            case 'X' : text[i] = 'M';
+            break;
+            case 'I' : text[i] = 'N';
+            break;
+            case 'C' : text[i] = 'O';
+            break;
+            case 'Y' : text[i] = 'P';
+            break;
+            case 'V' : text[i] = 'Q';
+            break;
+            case 'H' : text[i] = 'R';
+            break;
+            case 'B' : text[i] = 'S';
+            break;
+            case 'A' : text[i] = 'T';
+            break;
+            case 'E' : text[i] = 'U';
+            break;
+            case 'Q' : text[i] = 'V';
+            break;
+            case 'G' : text[i] = 'W';
+            break;
+            case 'M' : text[i] = 'X';
+            break;
+            case 'P' : text[i] = 'Y';
+            break;
+            case 'K' : text[i] = 'Z';
+            break;
+            case ' ' : text[i] = ' ';
+            break;
+            default : printf("Error with reading file\n");
+                        return;
+        }
+    }
+}
