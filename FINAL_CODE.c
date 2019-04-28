978cd23179e2698cc1bb7e2d4c7c33298da579d8
Sarah Boutchard <c3308022@uon.edu.au>
2019-04-28 12:15:15 +0000

    Removed switch case

--- FINAL_CODE.c
+++ FINAL_CODE.c
@@ -99,66 +99,18 @@ void sub_encrypt(char text[]) {
 void sub_decrypt(char text[]) {
     const char def[] = "TSOFUDWRNLZJXICYVHBAEQGMPK";
     for(int i=0; text[i] != '\0'; i++) {
-        switch (text[i]){
-            case 'T' : text[i] = 'A';
-            break;
-            case 'S' : text[i] = 'B';
-            break;
-            case 'O' : text[i] = 'C';
-            break;
-            case 'F' : text[i] = 'D';
-            break;
-            case 'U' : text[i] = 'E';
-            break;
-            case 'D' : text[i] = 'F';
-            break;
-            case 'W' : text[i] = 'G';
-            break;
-            case 'R' : text[i] = 'H';
-            break;
-            case 'N' : text[i] = 'I';
-            break;
-            case 'L' : text[i] = 'J';
-            break;
-            case 'Z' : text[i] = 'K';
-            break;
-            case 'J' : text[i] = 'L';
-            break;
-            case 'X' : text[i] = 'M';
-            break;
-            case 'I' : text[i] = 'N';
-            break;
-            case 'C' : text[i] = 'O';
-            break;
-            case 'Y' : text[i] = 'P';
-            break;
-            case 'V' : text[i] = 'Q';
-            break;
-            case 'H' : text[i] = 'R';
-            break;
-            case 'B' : text[i] = 'S';
-            break;
-            case 'A' : text[i] = 'T';
-            break;
-            case 'E' : text[i] = 'U';
-            break;
-            case 'Q' : text[i] = 'V';
-            break;
-            case 'G' : text[i] = 'W';
-            break;
-            case 'M' : text[i] = 'X';
-            break;
-            case 'P' : text[i] = 'Y';
-            break;
-            case 'K' : text[i] = 'Z';
-            break;
-            case ' ' : text[i] = ' ';
-            break;
-            default : printf("Error with reading file\n");
-                        return;
+        
+        if( text[i] >= 'A' && text[i] <= 'Z'){
+            text[i] = def[text[i] - 65];
+        }else if(text[i] == ' '){
+            //Nothing
+        }else{
+            printf("Error with reading file\n");
+            return;
         }
     }
 }
+
     void flush() { //buffer function
         int c;
         while ((c = getchar()) != '\n' && c != EOF && c != '\0') { }
